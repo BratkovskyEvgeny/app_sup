333c24d394b3a2b3a2f5a70a234b266bb94e41d3
Evgeny <120304735+BratkovskyEvgeny@users.noreply.github.com>
2023-11-01 14:59:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,25 +7,25 @@ from streamlit_player import st_player
 
 
     
-    st.image('1.JPG',width=710)
-    st.markdown("<h1 style='text-align: center; '>Благодарность</h1>", unsafe_allow_html = True)
-    st.balloons()
-    st.write("От всех обучающихся секретам Apache Superset выражаю благодарность за этот курс !!! ")
+st.image('1.JPG',width=710)
+st.markdown("<h1 style='text-align: center; '>Благодарность</h1>", unsafe_allow_html = True)
+st.balloons()
+st.write("От всех обучающихся секретам Apache Superset выражаю благодарность за этот курс !!! ")
     
-    st_player("https://www.youtube.com/watch?v=MdnE-7IpaD8")
+st_player("https://www.youtube.com/watch?v=MdnE-7IpaD8")
     
 
     
-    if st.button("Что дальше?"):
+if st.button("Что дальше?"):
         
        
-        st.write('Дальше - зима и время клепать дашборды, как пирожки :sunglasses:.')
-        time.sleep(2.5)
+    st.write('Дальше - зима и время клепать дашборды, как пирожки :sunglasses:.')
+    time.sleep(2.5)
         
         
-        st.snow()
-        time.sleep(3.5)
-        st_player("https://soundcloud.com/user-854788030/paul-hertzog-preparation-mp3")
+    st.snow()
+    time.sleep(3.5)
+    st_player("https://soundcloud.com/user-854788030/paul-hertzog-preparation-mp3")
         
         
     
